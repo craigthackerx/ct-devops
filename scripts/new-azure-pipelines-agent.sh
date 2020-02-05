72e741d03913c8bbaf336c9789371f3ff0911e58
Craig Thacker <34176957+craigthackerx@users.noreply.github.com>
2020-02-05 08:45:36 +0000

    Update new-azure-pipelines-agent.sh

--- scripts/new-azure-pipelines-agent.sh
+++ scripts/new-azure-pipelines-agent.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 AZURE_LINK="https://vstsagentpackage.azureedge.net/agent/2.164.6/vsts-agent-linux-x64-2.164.6.tar.gz"
+
 read -p 'Please enter your desired Agent Name...: ' AGENT_NAME && \
 echo "Downloading Azure Agents Package..." && \
 cd $HOME && wget ${AZURE_LINK} && \
 echo "Download Complete" && \
-mkdir $HOME/${AGENT_NAME} && chmod 777 -R $HOME/${AGENT_NAME} && cd $HOME/${AGENT_NAME} && \
+mkdir $HOME/${AGENT_NAME} && chmod 755 -R $HOME/${AGENT_NAME} && cd $HOME/${AGENT_NAME} && \
+
 echo "Unzipping Agent Package"
 tar zxvf $HOME/vsts-agent-linux-*.tar.gz && rm $HOME/vsts-agent-linux*.tar.gz && \
 cd $HOME/${AGENT_NAME} && \
