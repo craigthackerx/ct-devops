f0fe26a60fe466467c141e878596dcb1ebac6dbf
Craig Thacker <34176957+craigthackerx@users.noreply.github.com>
2020-04-17 12:33:51 +0100

    Create move-ssh-keys.sh

---
+++ scripts/bash/move-ssh-keys.sh
@@ -0,0 +1,6 @@
+read -p 'Please enter the username of the target machine: ' USERNAME && \ 
+read -p 'Please enter the IP of the target machine: ' TARGET_IP && \
+
+ssh-copy-id $USERNAME@$TARGET_IP && \
+scp ~/.ssh/* $USERNAME@$TARGET_IP:/home/$USERNAME/.ssh && \
+ssh -t $USERNAME@$TARGET_IP 'bash -i -c "chmod 700 ~/.ssh"'
